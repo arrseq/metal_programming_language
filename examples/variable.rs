36070002a4db040ce25e3a40e8ad23e7b819cf0c
Rayyan K <rayyankhan1428h@gmail.com>
2024-09-10 08:37:51 -0400

    feat(lexer) Implemented basic lexer.

--- examples/variable.rs
+++ examples/variable.rs
@@ -1,3 +1,36 @@
-fn main() {
+use colored::Colorize;
+use metal_programming_language::core::token;
+use metal_programming_language::core::token::Token;
 
+fn main() {
+    let mut tokens = token::Iterator::from(include_str!("./variable/math.mtx"));
+    
+    println!("Colored tokens view");
+    for token in tokens {
+        let token_string = String::from(token);
+        let painted = match token {
+            Token::Space => token_string.white(),
+            Token::Tab => token_string.white(),
+            Token::Newline => token_string.white(),
+            Token::Identifier(_) => token_string.red(),
+            Token::Digit(_) => token_string.cyan(),
+            Token::OpeningBracket => token_string.white(),
+            Token::ClosingBracket => token_string.white(),
+            Token::OpeningChevron => token_string.white(),
+            Token::ClosingChevron => token_string.white(),
+            Token::Path => token_string.bright_red(),
+            Token::Macro => token_string.bright_yellow(),
+            Token::Decimal => token_string.red(),
+            Token::Stop => token_string.red(),
+            Token::Separator => token_string.cyan(),
+            Token::Equal => token_string.cyan(),
+            Token::StringQuote => token_string.green(),
+            Token::CharacterQuote => token_string.green(),
+            Token::Escape => token_string.yellow(),
+            Token::Comment => token_string.bright_black(),
+            Token::Other(_) => token_string.magenta()
+        };
+        
+        print!("{}", painted);
+    }
 }
--- src/core/token.rs
+++ src/core/token.rs
@@ -2,7 +2,7 @@
 
 use std::iter;
 use std::iter::Peekable;
-use std::str::Chars;
+use std::str::CharIndices;
 
 #[derive(Debug, Clone, Copy, PartialEq, Default)]
 pub enum Token<'a> {
@@ -18,6 +18,7 @@ pub enum Token<'a> {
     OpeningChevron,
     ClosingChevron,
 
+    Path,
     Macro,
     Decimal,
     Stop,
@@ -25,7 +26,37 @@ pub enum Token<'a> {
     Equal,
     StringQuote,
     CharacterQuote,
-    Escape
+    Escape,
+    Comment,
+
+    Other(char)
+}
+
+impl<'a> From<Token<'a>> for String {
+    fn from(value: Token<'a>) -> Self {
+        match value {
+            Token::Space => String::from(" "),
+            Token::Tab => String::from("\t"),
+            Token::Newline => String::from("\n"),
+            Token::Identifier(identifier) => String::from(identifier),
+            Token::Digit(digit) => format!("{}", digit),
+            Token::OpeningBracket => String::from("["),
+            Token::ClosingBracket => String::from("]"),
+            Token::OpeningChevron => String::from("<"),
+            Token::ClosingChevron => String::from(">"),
+            Token::Path => String::from(":"),
+            Token::Macro => String::from("#"),
+            Token::Decimal => String::from(","),
+            Token::Stop => String::from("."),
+            Token::Separator => String::from("|"),
+            Token::Equal => String::from("="),
+            Token::StringQuote => String::from("'"),
+            Token::CharacterQuote => String::from("\""),
+            Token::Escape => String::from("\\"),
+            Token::Comment => String::from("/"),
+            Token::Other(other) => String::from(other)
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -35,12 +66,15 @@ pub struct Mapping<'a> {
 }
 
 impl<'a> Token<'a> {
-    pub const MAPPINGS: [Mapping<'a>; 13] = [
+    pub const MAPPINGS: [Mapping<'a>; 16] = [
         Mapping { character: ' ',  token: Token::Space          },
+        Mapping { character: '\t', token: Token::Tab            },
+        Mapping { character: '\n', token: Token::Newline        },
         Mapping { character: '[',  token: Token::OpeningBracket },
         Mapping { character: ']',  token: Token::ClosingBracket },
         Mapping { character: '<',  token: Token::OpeningChevron },
         Mapping { character: '>',  token: Token::ClosingChevron },
+        Mapping { character: ':',  token: Token::Path           },
         Mapping { character: '#',  token: Token::Macro          },
         Mapping { character: ',',  token: Token::Decimal        },
         Mapping { character: '.',  token: Token::Stop           },
@@ -48,19 +82,19 @@ impl<'a> Token<'a> {
         Mapping { character: '=',  token: Token::Equal          },
         Mapping { character: '"',  token: Token::StringQuote    },
         Mapping { character: '\'', token: Token::CharacterQuote },
-        Mapping { character: '\\', token: Token::Escape         }
+        Mapping { character: '/',  token: Token::Comment        }
     ];
 }
 
 #[derive(Debug, Clone)]
 pub struct Iterator<'a> {
     source: &'a str,
-    chars: Peekable<Chars<'a>>
+    chars: Peekable<CharIndices<'a>>
 }
 
 impl<'a> From<&'a str> for Iterator<'a> {
     fn from(value: &'a str) -> Self {
-        Self { source: value, chars: iter::Iterator::peekable(value.chars()) }
+        Self { source: value, chars: iter::Iterator::peekable(value.char_indices()) }
     }
 }
 
@@ -69,8 +103,25 @@ impl<'a> iter::Iterator for Iterator<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         let peeked = self.chars.peek()?;
-        Token::MAPPINGS.iter().find(|item| item.character == peeked)?;
+        if let Some(mapped) = Token::MAPPINGS.iter().find(|item| item.character == peeked.1) {
+            let _ = self.chars.next();
+            return Some(mapped.token)
+        }
+        if let Some(digit) = peeked.1.to_digit(10) {
+            let _ = self.chars.next();
+            return Some(Token::Digit(digit as u8))
+        }
         
-        todo!()
+        let byte_start = peeked.0;
+        let mut byte_end = byte_start;
+
+        while let Some(character) = self.chars.peek() {
+            if !character.1.is_alphabetic() && !character.1.is_alphanumeric() && character.1 != '_' { break }
+            byte_end += character.1.len_utf8();
+            let _ = self.chars.next();
+        }
+
+        if byte_end == byte_start { return Some(Token::Other(self.chars.next()?.1)) }
+        Some(Token::Identifier(&self.source[byte_start..byte_end]))
     }
 }
